95bd18bed1ef9a051561d2562a33ee8fdd23dffb
Balagopal Kanattil <balukanattil@gmail.com>
2024-01-07 13:47:12 -0500

    Fix home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,5 +24,21 @@ export default {
 	},
 	plugins: [
 		require("@tailwindcss/typography"),
+		plugin(function ({ addComponents }) {
+			addComponents({
+				".project-link": {
+					"@apply bg-[size:100%_6px] bg-bottom bg-repeat-x": {},
+					backgroundImage:
+						"linear-gradient(transparent,transparent 5px,hsl(var(--theme-text)) 5px,hsl(var(--theme-text)))",
+					"&:hover": {
+						backgroundImage:
+							"linear-gradient(transparent,transparent 4px,hsl(var(--theme-link)) 4px,hsl(var(--theme-link)))",
+					},
+				},
+				".title": {
+					"@apply text-2xl font-semibold text-accent-2": {},
+				},
+			});
+		}),
 	],
 }
