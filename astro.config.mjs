25f9d966e8c9ad1397507c6bd91d70d9aa92e64b
Balagopal Kanattil <balukanattil@gmail.com>
2024-02-18 11:30:01 -0500

    Improvements to post page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kanatti.github.io',
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), mdx()]
 });
