9ae6ac26cc760d20878953f298e503d9c9739058
Balagopal Kanattil <balukanattil@gmail.com>
2026-05-09 19:42:41 +0000

    blog moved page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kanatti.github.io',
-  redirects: {
-    '/blog': '/posts',
-    '/blog/[...slug]': '/posts/[...slug]',
-  },
   integrations: [tailwind(), icon(), mdx()],
   markdown: {
     shikiConfig: {
